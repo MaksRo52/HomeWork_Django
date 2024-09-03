c2f14bdb178713fdd22fe3062b5f735178c85148
MaksRo <romancov_dzr@mail.ru>
2024-09-03 14:56:52 +0300

    base add

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_base',
+        'USER': 'postgres',
+        'PASSWORD': '123qwe$asd',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
