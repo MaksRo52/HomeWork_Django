21fee5ddb41e882033fdbe72f2f5fc2a55169884
MaksRo <romancov_dzr@mail.ru>
2024-09-07 17:49:58 +0300

    create commands

--- catalog/management/commands/new_data.py
+++ catalog/management/commands/new_data.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read_categories():  # Здесь мы получаем данные из фикстур с категориями
-        with open("fixture/Catalog_data.json", encoding="utf-8") as file:
+        with open("catalog/fixture/Catalog_data.json", "r", encoding="utf-8") as file:
             values = json.load(file)
         categories = [value for value in values if value['model'] == "catalog.category"]
         return categories
@@ -15,7 +15,7 @@ class Command(BaseCommand):
     @staticmethod
     def json_read_products():  # Здесь мы получаем данные из фикстур с продуктами
 
-        with open("fixture/Catalog_data.json", encoding="utf-8") as file:
+        with open("catalog/fixture/Catalog_data.json", "r", encoding="utf-8") as file:
             values = json.load(file)
         categories = [value for value in values if value['model'] == "catalog.product"]
         return categories
