8c37c811d69e04bbd591df4909167b0d3f0bd55f
MaksRo <romancov_dzr@mail.ru>
2024-09-09 17:50:55 +0300

    create

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
 
 
 def contacts(request):
