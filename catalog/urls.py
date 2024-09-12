fc83865337e3845e804d5a7efc0b6b9e6c832d04
MaksRo <romancov_dzr@mail.ru>
2024-09-12 17:24:20 +0300

    create file, add CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 
 from catalog.apps import CatalogConfig
 from django.urls import path
-from catalog.views import home, contacts, product_info
+from catalog.views import ProductListView, contacts, product_info
 
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('product/<int:pk>', product_info, name='product_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from catalog.models import Product
 
 
 # Create your views here.
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'products_list.html', context)
+# def home(request):
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(request, 'products_list.html', context)
+
+class ProductListView(ListView):
+    model = Product
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
 
 
 def product_info(request, pk):
     product = Product.objects.get(pk=pk)
     context = {"product": product}
-    return render(request, 'product_info.html', context)
+    return render(request, 'catalog/product_info.html', context)
