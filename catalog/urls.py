be4d98802b78feeb63e4ad5832d6a08d1a6fcc9c
MaksRo <romancov_dzr@mail.ru>
2024-09-12 17:41:49 +0300

    create CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 
 from catalog.apps import CatalogConfig
 from django.urls import path
-from catalog.views import ProductListView, contacts, product_info
+from catalog.views import ProductListView, ContactsView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>', product_info, name='product_info')
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views import generic
 
 from catalog.models import Product
 
 
-# Create your views here.
-# def home(request):
-#     products = Product.objects.all()
-#     context = {"products": products}
-#     return render(request, 'products_list.html', context)
-
-class ProductListView(ListView):
+class ProductListView(generic.ListView):
     model = Product
 
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
+class ContactsView(generic.TemplateView):
+    template_name = 'catalog/contacts.html'
 
 
-def product_info(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {"product": product}
-    return render(request, 'catalog/product_info.html', context)
+class ProductDetailView(generic.DetailView):
+    model = Product
+
