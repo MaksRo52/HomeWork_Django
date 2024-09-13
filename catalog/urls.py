36d63cf2c8d14b188f87cc0b4455b90ab98a8b07
m.romantsov <romancov_dzr@mail.ru>
2024-09-13 17:43:11 +0300

    Добавил урл на создание категории

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 
 from catalog.apps import CatalogConfig
 from django.urls import path, include
-from catalog.views import ProductListView, ContactsView, ProductDetailView, ProductCreateView
+from catalog.views import ProductListView, ContactsView, ProductDetailView, ProductCreateView, CategoryCreateView
 
 app_name = CatalogConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product_info'),
+    path('cr_cat/', CategoryCreateView.as_view(), name='category'),
 ]
