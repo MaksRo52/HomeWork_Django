64281781b2e148dff0b64c04243436c23d73de3a
MaksRo <romancov_dzr@mail.ru>
2024-09-23 21:59:10 +0300

    task 3 (StyleFormMixin)

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
 
 from catalog.models import Product, Version, Category
 
@@ -7,7 +7,10 @@ class StyleFormMixin(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            pass
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
 
 
 class ProductForm(StyleFormMixin, ModelForm):
